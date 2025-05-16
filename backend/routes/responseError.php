96767a5ac505b853c119061a7909640086a6028e
JuanMartinCoder <skyzinformatica@gmail.com>
2025-05-15 21:20:15 -0300

    server.php refactor

---
+++ backend/routes/responseError.php
@@ -0,0 +1,7 @@
+<?php
+  
+http_response_code(404);
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode(["error" => "Error ruta desconocida"]);
+
+?>
--- backend/server.php
+++ backend/server.php
@@ -9,10 +9,22 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
 
-require_once("./routes/studentsRoutes.php");
+$request_raw = $_SERVER["REQUEST_URI"];
+$request_clean = str_replace("/ProyectoCrudRefact/backend","",$request_raw);
+
+switch ($request_clean) {
+  case '/students':
+    require_once("./routes/studentsRoutes.php");
+    break;
+  default:
+    require_once("./routes/responseError.php");
+    break;
+}
+
 ?>
